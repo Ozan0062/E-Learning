692b25988556fd929f5b48045beca38b5be8913b
silasgraso <silas.graso@gmail.com>
2023-05-01 14:06:41 +0200

    New folder and class

---
+++ Pages/Silas/testclass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Learning.Pages.Silas
+{
+    public class testclassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
