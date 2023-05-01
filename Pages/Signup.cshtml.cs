37345b46020179da3d355bb6b5ddadc3f216ee2e
Ozan0062 <ozan0062@edu.zealand.dk>
2023-05-01 13:33:10 +0200

    Add project files.

---
+++ Pages/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Learning.Pages
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
