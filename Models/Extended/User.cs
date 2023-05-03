2b38175560e45d1ac0df6afad4469ecf8c0e1f29
Ozan0062 <ozan0062@edu.zealand.dk>
2023-05-03 11:27:54 +0200

    New

---
+++ Models/Extended/User.cs
@@ -0,0 +1,10 @@
+﻿
+public partial class User : IHasId, IUpdateFromOther<User>
+{
+    public void Update(User tOther)
+    {
+        Email = tOther.Email;
+        Password = tOther.Password;
+    }
+}
+
