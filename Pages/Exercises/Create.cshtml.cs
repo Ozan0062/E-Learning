b0b50db92db5c653dfd5af8440967af83dabfa75
Ozan0062 <ozan0062@edu.zealand.dk>
2023-05-17 11:10:42 +0200

    Profil

--- Pages/Exercises/Create.cshtml.cs
+++ Pages/Exercises/Create.cshtml.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace E_Learning.Pages.Exercises
 {
     public class CreateModel : CreatePageModel<Exercise>
     {
-        public CreateModel(IExerciseDataService dataService)
+        public CreateModel(IExerciseDataService dataService, ICourseDataService courseDataService)
             : base(dataService)
         {
-        }
-    }
+			CourseList = new SelectList(courseDataService.GetAll(), nameof(Course.Id), nameof(Course.Name));
+	    }
+
+	public SelectList CourseList { get; set; }
+}
 }
