9ebce576f03d797d294a785a696fdea90c6f8c61
silasgraso <silas.graso@gmail.com>
2023-05-25 11:45:57 +0200

    k

--- Services/Data/EFCCourseDataService.cs
+++ Services/Data/EFCCourseDataService.cs
@@ -2,8 +2,12 @@
 
 public class EFCCourseDataService : EFCDataServiceAppBase<Course>, ICourseDataService
 {
-	protected override IQueryable<Course> GetAllWithIncludes(DbContext context)
-	{
-		return context.Set<Course>();
-	}
+
+
+    protected override IQueryable<Course> GetAllWithIncludes(DbContext context)
+    {
+        return context.Set<Course>().Include(c => c.Exercises);
+    }
+
+
 }
